5a5b7966f790155f2ef5d3c289d7a739c24d0277
Anshul Dubey <dubeyanshul2204@gmail.com>
2025-06-03 20:23:51 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+//! Create a Database Connection
+mongoose
+  .connect(
+    "mongodb+srv://dubeyanshul2204:KylGPOyqHd3zN9kx@cluster0.ng0yraz.mongodb.net/"
+  )
+  .then(() => {
+    console.log("MongoDB is Connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+//! Create a App
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//! Configure the cors
+//? Hey server, allow requests from my frontend app running at localhost:5173. Let it send or receive data, including login info, and allow specific request types and headers
+
+//? app.use is simply app uses
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "content-type",
+      "Authorization",
+      "Cache-control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+//! Run the Server
+app.listen(() => {
+  console.log(`Server is now running in port ${PORT}`);
+});
